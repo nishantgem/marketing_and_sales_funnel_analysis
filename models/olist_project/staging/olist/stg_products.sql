c3e570181d84338785e174e3ab4580238119e019
nishantgem <nishant.gem@gmail.com>
2026-01-22 13:17:56 +0100

    Fix folder structure for Olist models

--- models/olist_project/staging/olist/stg_products.sql
+++ models/olist_project/staging/olist/stg_products.sql
@@ -7,8 +7,13 @@ renamed as (
     select
         product_id,
         product_category_name,
-        product_name_length,
-        product_description_length,
+
+        -- raw column is misspelled: product_name_lenght
+        product_name_lenght as product_name_length,
+
+        -- raw column is misspelled: product_description_lenght
+        product_description_lenght as product_description_length,
+
         product_photos_qty,
         product_weight_g,
         product_length_cm,
